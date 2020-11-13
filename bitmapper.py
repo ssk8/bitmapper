565e8404f749fe2964ace78d819ddbe2a310bcd4
ssk8 <stinkyskate@pm.me>
2020-11-12 20:46:26 -0600

    added QR func and some cleanup

--- bitmapper.py
+++ bitmapper.py
@@ -1,4 +1,7 @@
-import sys, math, colorsys
+#!/usr/bin/python3
+
+import colorsys
+import math
 import click
 from PIL import Image
 
@@ -6,53 +9,63 @@ from PIL import Image
 @click.option('--threshold', default=127, help='Brightness value at which pixels will be on')
 @click.option('--show-output/--no-show-output', default=True, help='Show output image')
 @click.option('--invert/--no-invert', default=False, help='Invert output image')
-@click.option('--new_dim', default=False, help='change output dimentions')
+@click.option('--resize', default=False, help='change output dimentions')
 @click.option('--rot', default=False, help='rotate by')
-@click.argument('image_file')
-def convert(image_file, threshold, show_output, invert, new_dim, rot):
+@click.option('--qr', default=False, help='make "start-point" qr-code')
+@click.argument('start_point')
+def convert(start_point, threshold, show_output, invert, resize, rot, qr):
     """
-    img2h will convert bitmaps to C/C++ headers with pixels represented
-    as byte arrays.
+    convert bitmaps to C/C++ headers with pixels represented as byte arrays.
     """
-    im = Image.open(image_file)
+    if qr:
+        import qrcode
+        im = qrcode.make(start_point)
+        start_point = "qrcode._"
+    else:
+        im = Image.open(start_point)
     if rot:
         im = im.rotate(int(rot))
-    if new_dim:
-        im = im.resize([int(n) for n in new_dim.split()])
+    if resize:
+        im = im.resize([int(n) for n in resize.split()])
     im.show()
 
     width, height = im.size
     width = int(width)
     height = int(height)
     columns = int(math.ceil(width / 8.0))
-    name, ext = image_file.split('.')
+
+    name, _ = start_point.split('.')
+
     with open(name + '.h', 'w') as header:
         name = 'pic'
         header.write(f"int bmpWidth = {width}, bmpHeight = {height};\n")
         header.write("static const unsigned char " + name + "[] = {\n")
 
-        for y in range(0, height):
+        for y in range(height):
             header.write("\t")
 
-            for column in range(0, columns):
+            for column in range(columns):
                 pix_byte = ''
-                for bit in range(0, 8):
+                for bit in range(8):
                     x = column * 8 + bit
                     char = "0"
                     if x < width:
-                        pixel = colorsys.rgb_to_hsv(*im.getpixel((x, y))[:3])
+                        if hasattr(im.getpixel((x, y)), "__iter__"):
+                            pixel = colorsys.rgb_to_hsv(*im.getpixel((x, y))[:3])
+                        else:
+                            pixel = [0,0,im.getpixel((x, y))]
                         if pixel[2] < threshold:
                             char = "1"
                     if invert:
                         char = "1" if char == "0" else "0"
                     pix_byte += char
                     if show_output:
-                        sys.stdout.write(" " if char == "0" else "#")
+                        print(" " if char == "0" else "#", end ="")
                 header.write(f'{int(pix_byte,2):#x}')
                 header.write(", ")
             header.write("\n")
             if show_output:
-                sys.stdout.write("\n")
+                print()
 
         header.write("};\n\n")
 
