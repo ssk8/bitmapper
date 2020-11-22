f3a650896174ad63033e9aa925d78d7320705ba2
ssk8 <stinkyskate@pm.me>
2020-11-21 20:08:43 -0600

    cleanup

--- bitmapper.py
+++ bitmapper.py
@@ -4,18 +4,19 @@ import click
 
 
 @click.command()
-@click.option('--threshold', default=127, help='Brightness value at which pixels will be on')
-@click.option('--show-output/--no-show-output', default=True, help='Show output image')
-@click.option('--invert/--no-invert', default=False, help='Invert output image')
+@click.option('-s', is_flag=True, help='Show output image')
 @click.option('-r', is_flag=True, help='rotate by 90')
 @click.option('-qr', is_flag=True, help='make "start-point" qr-code')
 @click.option('-h', is_flag=True, help='make C/C++ header')
 @click.argument('start_point')
 @click.argument('size')
-def convert(start_point, size, threshold, show_output, invert, r, qr, h):
+def convert(start_point, size, s, r, qr, h, ):
     """
     optimize images for monocrome displays including C/C++ headers with pixels represented as byte arrays.
-    size should be in "wxh"
+    usage: bitmapper img_file.image_format hxw  where h and w are the desired hight and width of output.
+    e.g. bitmapper sample.jpg 250x122
+    or to generate QR code:
+    bitmapper -qr QRmessage 250x122
     """
 
     if qr:
@@ -29,12 +30,12 @@ def convert(start_point, size, threshold, show_output, invert, r, qr, h):
         im = im.transpose(Image.ROTATE_90)
     im = im.resize([int(n) for n in size.split("x")])
     im = im.convert(mode="1", dither=Image.FLOYDSTEINBERG)
-    im.show()
+    if s: im.show()
     im.save(img_file.with_name(f"{img_file.stem}_{im.size[0]}x{im.size[1]}.jpg"), "JPEG")
-    if h: make_header(im, threshold, img_file)
+    if h: make_header(im, img_file)
 
 
-def make_header(im: Image, threshold, img_file: Path, invert=False, show_output=False, ):
+def make_header(im: Image, img_file: Path, ):
     width, height = im.size
     im = ImageOps.invert(im.convert('L')).convert('1')
     with img_file.with_name(f"{img_file.stem}.h").open('w') as header:
