a18eb8fee81608af2c7d34eda5c0265662511958
ssk8 <stinkyskate@pm.me>
2020-11-21 19:46:40 -0600

    let pillow do the work

--- bitmapper.py
+++ bitmapper.py
@@ -1,74 +1,49 @@
-#!/usr/bin/python3
-
-import colorsys
-import math
+from PIL import Image, ImageOps
+from pathlib import Path
 import click
-from PIL import Image
+
 
 @click.command()
 @click.option('--threshold', default=127, help='Brightness value at which pixels will be on')
 @click.option('--show-output/--no-show-output', default=True, help='Show output image')
 @click.option('--invert/--no-invert', default=False, help='Invert output image')
-@click.option('--resize', default=False, help='change output dimentions')
-@click.option('--rot', default=False, help='rotate by')
+@click.option('-r', is_flag=True, help='rotate by 90')
 @click.option('-qr', is_flag=True, help='make "start-point" qr-code')
+@click.option('-h', is_flag=True, help='make C/C++ header')
 @click.argument('start_point')
-def convert(start_point, threshold, show_output, invert, resize, rot, qr):
+@click.argument('size')
+def convert(start_point, size, threshold, show_output, invert, r, qr, h):
     """
-    convert bitmaps to C/C++ headers with pixels represented as byte arrays.
+    optimize images for monocrome displays including C/C++ headers with pixels represented as byte arrays.
+    size should be in "wxh"
     """
+
     if qr:
         import qrcode
         im = qrcode.make(start_point)
-        start_point = "qrcode._"
+        img_file = Path("qrcode")
     else:
+        img_file = Path(start_point)
         im = Image.open(start_point)
-    if rot:
-        im = im.rotate(int(rot))
-    if resize:
-        im = im.resize([int(n) for n in resize.split()])
+    if r:
+        im = im.transpose(Image.ROTATE_90)
+    im = im.resize([int(n) for n in size.split("x")])
     im = im.convert(mode="1", dither=Image.FLOYDSTEINBERG)
     im.show()
+    im.save(img_file.with_name(f"{img_file.stem}_{im.size[0]}x{im.size[1]}.jpg"), "JPEG")
+    if h: make_header(im, threshold, img_file)
 
+
+def make_header(im: Image, threshold, img_file: Path, invert=False, show_output=False, ):
     width, height = im.size
-    width = int(width)
-    height = int(height)
-    columns = int(math.ceil(width / 8.0))
-
-    name, _ = start_point.split('.')
-
-    with open(name + '.h', 'w') as header:
-        name = 'pic'
+    im = ImageOps.invert(im.convert('L')).convert('1')
+    with img_file.with_name(f"{img_file.stem}.h").open('w') as header:
         header.write(f"int bmpWidth = {width}, bmpHeight = {height};\n")
-        header.write("static const unsigned char " + name + "[] = {\n")
-
-        for y in range(height):
-            header.write("\t")
-
-            for column in range(columns):
-                pix_byte = ''
-                for bit in range(8):
-                    x = column * 8 + bit
-                    char = "0"
-                    if x < width:
-                        if hasattr(im.getpixel((x, y)), "__iter__"):
-                            pixel = colorsys.rgb_to_hsv(*im.getpixel((x, y))[:3])
-                        else:
-                            pixel = [0,0,im.getpixel((x, y))]
-                        if pixel[2] < threshold:
-                            char = "1"
-                    if invert:
-                        char = "1" if char == "0" else "0"
-                    pix_byte += char
-                    if show_output:
-                        print(" " if char == "0" else "#", end ="")
-                header.write(f'{int(pix_byte,2):#x}')
-                header.write(", ")
-            header.write("\n")
-            if show_output:
-                print()
-
+        header.write(f"static const unsigned char {img_file.stem}[] = {{\n")
+        for b in im.tobytes():
+            header.write(f"{b:#x}, ")
         header.write("};\n\n")
+
 
 if __name__ == '__main__':
     convert()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='bitmapper',
     version="0.4",
     py_modules=['convert'],
-    install_requires=['click', 'pillow'],
+    install_requires=['click', 'pillow', 'qrcode'],
     entry_points='''
         [console_scrips]
         convert=convert:cli
